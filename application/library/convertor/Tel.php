3ecb01a9593cf9340546814fab9498e7d2c33afe
zhangxm <zhangxm@youpu.me>
2017-05-05 19:38:49 +0800

    电话黄页管理

--- application/library/convertor/Tel.php
+++ application/library/convertor/Tel.php
@@ -14,7 +14,7 @@ class Convertor_Tel extends Convertor_Base {
         $data = array(
             'list' => array()
         );
-        
+
         $telListByType = array();
         foreach ($telList as $tel) {
             $telListByType[$tel['typeid']][] = array(
@@ -23,7 +23,7 @@ class Convertor_Tel extends Convertor_Base {
                 'tel' => $tel['tel']
             );
         }
-        
+
         foreach ($telTypeList as $type) {
             $typeTemp = array();
             $typeTemp['id'] = $type['id'];
@@ -31,6 +31,36 @@ class Convertor_Tel extends Convertor_Base {
             $typeTemp['telList'] = $telListByType[$type['id']] ? $telListByType[$type['id']] : array();
             $data['list'][] = $typeTemp;
         }
+        return $data;
+    }
+
+    public function getTelListConvertor($list, $count, $param) {
+        $data = array(
+            'list' => array()
+        );
+
+        $typeIdList = array_unique(array_column($list, 'typeid'));
+        $telTypeModel = new TelTypeModel();
+        $typeInfoList = $telTypeModel->getTelTypeList(array('id' => $typeIdList));
+        $typeNameList = array_column($typeInfoList, 'title_lang1', 'id');
+
+        foreach ($list as $key => $value) {
+            $oneTemp = array();
+            $oneTemp['id'] = $value['id'];
+            $oneTemp['title_lang1'] = $value['title_lang1'];
+            $oneTemp['title_lang2'] = $value['title_lang2'];
+            $oneTemp['title_lang3'] = $value['title_lang3'];
+            $oneTemp['tel'] = $value['tel'];
+            $oneTemp['status'] = $value['status'];
+            $oneTemp['typeid'] = $value['typeid'];
+            $oneTemp['typeName'] = $typeNameList[$value['typeid']];
+            $oneTemp['hotelid'] = $value['hotelid'];
+            $data['list'][] = $oneTemp;
+        }
+        $data['total'] = $count;
+        $data['page'] = $param['page'];
+        $data['limit'] = $param['limit'];
+        $data['nextPage'] = Util_Tools::getNextPage($data['page'], $data['limit'], $data['total']);
         return $data;
     }
 }
--- application/library/dao/TelType.php
+++ application/library/dao/TelType.php
@@ -8,7 +8,7 @@ class Dao_TelType extends Dao_Base {
 
     /**
      * 查询hotel_tel_type列表
-     * 
+     *
      * @param
      *            array 入参
      * @return array
@@ -16,50 +16,85 @@ class Dao_TelType extends Dao_Base {
     public function getTelTypeList(array $param): array {
         $limit = $param['limit'] ? intval($param['limit']) : 0;
         $page = $this->getStart($param['page'], $limit);
-        
+
+        $paramSql = $this->handlerListParams($param);
+        $sql = "select * from hotel_tel_type {$paramSql['sql']}";
+        if ($limit) {
+            $sql .= " limit {$page},{$limit}";
+        }
+        $result = $this->db->fetchAll($sql, $paramSql['case']);
+        return is_array($result) ? $result : array();
+    }
+
+    /**
+     * 查询hotel_tel_type数量
+     *
+     * @param
+     *            array 入参
+     * @return array
+     */
+    public function getTelTypeCount(array $param): int {
+        $paramSql = $this->handlerListParams($param);
+        $sql = "select count(1) as count from hotel_tel_type {$paramSql['sql']}";
+        $result = $this->db->fetchAssoc($sql, $paramSql['case']);
+        return intval($result['count']);
+    }
+
+    private function handlerListParams($param) {
         $whereSql = array();
         $whereCase = array();
+        if (isset($param['id'])) {
+            if (is_array($param['id'])) {
+                $whereSql[] = 'id in (' . implode(',', $param['id']) . ')';
+            } else {
+                $whereSql[] = 'id = ?';
+                $whereCase[] = $param['id'];
+            }
+        }
         if (isset($param['hotelid'])) {
             $whereSql[] = 'hotelid = ?';
             $whereCase[] = $param['hotelid'];
+        }
+        if (isset($param['title'])) {
+            $whereSql[] = '(title_lang1 = ? or title_lang2 = ? or title_lang3 = ?)';
+            $whereCase[] = $param['title'];
+            $whereCase[] = $param['title'];
+            $whereCase[] = $param['title'];
         }
         if (isset($param['islogin'])) {
             $whereSql[] = 'islogin = ?';
             $whereCase[] = $param['islogin'];
         }
         $whereSql = $whereSql ? ' where ' . implode(' and ', $whereSql) : '';
-        
-        $sql = "select * from hotel_tel_type {$whereSql}";
-        if ($limit) {
-            $sql .= " limit {$page},{$limit}";
-        }
-        $result = $this->db->fetchAll($sql, $whereCase);
-        return is_array($result) ? $result : array();
+        return array(
+            'sql' => $whereSql,
+            'case' => $whereCase
+        );
     }
 
     /**
      * 根据id查询hotel_tel_type详情
-     * 
+     *
      * @param
      *            int id
      * @return array
      */
     public function getTelTypeDetail(int $id): array {
         $result = array();
-        
+
         if ($id) {
             $sql = "select * from hotel_tel_type where id=?";
             $result = $this->db->fetchAssoc($sql, array(
                 $id
             ));
         }
-        
+
         return $result;
     }
 
     /**
      * 根据id更新hotel_tel_type
-     * 
+     *
      * @param
      *            array 需要更新的数据
      * @param
@@ -68,17 +103,17 @@ class Dao_TelType extends Dao_Base {
      */
     public function updateTelTypeById(array $info, int $id) {
         $result = false;
-        
+
         if ($id) {
-            $result = $this->db->update('hotel_tel_type', $info, $id);
+            $result = $this->db->update('hotel_tel_type', $info, array('id' => $id));
         }
-        
+
         return $result;
     }
 
     /**
      * 单条增加hotel_tel_type数据
-     * 
+     *
      * @param
      *            array
      * @return int id
