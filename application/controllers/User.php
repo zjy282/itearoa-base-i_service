55b018c53f2d22f05341f43961dfadbb888bf955
zhangxm <zhangxm@youpu.me>
2017-06-26 18:24:40 +0800

    获取物业所在集团端口

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,193 +1,195 @@
 <?php
+
 /**
  * 酒店入住用户控制器类
  *
  */
 class UserController extends \BaseController {
 
-	/**
-	 *
-	 * @var UserModel
-	 */
-	private $model;
+    /**
+     *
+     * @var UserModel
+     */
+    private $model;
 
-	/**
-	 *
-	 * @var Convertor_User
-	 */
-	private $convertor;
+    /**
+     *
+     * @var Convertor_User
+     */
+    private $convertor;
 
-	public function init() {
-		parent::init ();
-		$this->model = new UserModel ();
-		$this->convertor = new Convertor_User ();
-	}
+    public function init() {
+        parent::init();
+        $this->model = new UserModel ();
+        $this->convertor = new Convertor_User ();
+    }
 
-	/**
-	 * 获取酒店入住用户列表
-	 *
-	 * @return Json
-	 */
-	public function getUserListAction() {
-		$param = array ();
-		$param ['page'] = intval ( $this->getParamList ( 'page' ) );
-		$param ['limit'] = intval ( $this->getParamList ( 'limit', 5 ) );
-		$param ['id'] = intval ( $this->getParamList ( 'id' ) );
-		$param ['room_no'] = $this->getParamList ( 'room_no' );
-		$param ['fullname'] = $this->getParamList ( 'fullname' );
-		$param ['hotelid'] = intval ( $this->getParamList ( 'hotelid' ) );
-		$param ['groupid'] = intval ( $this->getParamList ( 'groupid' ) );
-		$param ['oid'] = $this->getParamList ( 'oid' );
-		$data = $this->model->getUserList ( $param );
-		$count = $this->model->getUserCount ( $param );
-		$data = $this->convertor->getUserListConvertor ( $data, $count, $param );
-		$this->echoSuccessData ( $data );
-	}
+    /**
+     * 获取酒店入住用户列表
+     *
+     * @return Json
+     */
+    public function getUserListAction() {
+        $param = array();
+        $param ['page'] = intval($this->getParamList('page'));
+        $param ['limit'] = intval($this->getParamList('limit', 5));
+        $param ['id'] = intval($this->getParamList('id'));
+        $param ['room_no'] = $this->getParamList('room_no');
+        $param ['fullname'] = $this->getParamList('fullname');
+        $param ['hotelid'] = intval($this->getParamList('hotelid'));
+        $param ['groupid'] = intval($this->getParamList('groupid'));
+        $param ['oid'] = $this->getParamList('oid');
+        $data = $this->model->getUserList($param);
+        $count = $this->model->getUserCount($param);
+        $data = $this->convertor->getUserListConvertor($data, $count, $param);
+        $this->echoSuccessData($data);
+    }
 
-	/**
-	 * 根据id获取酒店入住用户详情
-	 *
-	 * @param
-	 *        	int id 获取详情信息的id
-	 * @return Json
-	 */
-	public function getUserDetailAction() {
-		$id = intval ( $this->getParamList ( 'id' ) );
-		if ($id) {
-			$data = $this->model->getUserDetail ( $id );
-			$data = $this->convertor->getUserDetail ( $data );
-		} else {
-			$this->throwException ( 1, '查询条件错误，id不能为空' );
-		}
-		$this->echoJson ( $data );
-	}
+    /**
+     * 根据id获取酒店入住用户详情
+     *
+     * @param
+     *            int id 获取详情信息的id
+     * @return Json
+     */
+    public function getUserDetailAction() {
+        $id = intval($this->getParamList('id'));
+        if ($id) {
+            $data = $this->model->getUserDetail($id);
+            $data = $this->convertor->getUserDetail($data);
+        } else {
+            $this->throwException(1, '查询条件错误，id不能为空');
+        }
+        $this->echoJson($data);
+    }
 
-	/**
-	 * 根据id修改酒店入住用户信息
-	 *
-	 * @param
-	 *        	int id 获取详情信息的id
-	 * @param
-	 *        	array param 需要更新的字段
-	 * @return Json
-	 */
-	public function updateUserByIdAction() {
-		$id = intval ( $this->getParamList ( 'id' ) );
-		if ($id) {
-			$param = array ();
-			$param ['name'] = trim ( $this->getParamList ( 'name' ) );
-			$data = $this->model->updateUserById ( $param, $id );
-			$data = $this->convertor->commonConvertor ( $data );
-		} else {
-			$this->throwException ( 1, 'id不能为空' );
-		}
-		$this->echoJson ( $data );
-	}
+    /**
+     * 根据id修改酒店入住用户信息
+     *
+     * @param
+     *            int id 获取详情信息的id
+     * @param
+     *            array param 需要更新的字段
+     * @return Json
+     */
+    public function updateUserByIdAction() {
+        $id = intval($this->getParamList('id'));
+        if ($id) {
+            $param = array();
+            $param ['name'] = trim($this->getParamList('name'));
+            $data = $this->model->updateUserById($param, $id);
+            $data = $this->convertor->commonConvertor($data);
+        } else {
+            $this->throwException(1, 'id不能为空');
+        }
+        $this->echoJson($data);
+    }
 
-	/**
-	 * 添加酒店入住用户信息
-	 *
-	 * @param
-	 *        	array param 需要新增的信息
-	 * @return Json
-	 */
-	public function addUserAction() {
-		$param = array ();
-		$param ['name'] = trim ( $this->getParamList ( 'name' ) );
-		$data = $this->model->addUser ( $param );
-		$data = $this->convertor->commonConvertor ( $data );
-		$this->echoJson ( $data );
-	}
+    /**
+     * 添加酒店入住用户信息
+     *
+     * @param
+     *            array param 需要新增的信息
+     * @return Json
+     */
+    public function addUserAction() {
+        $param = array();
+        $param ['name'] = trim($this->getParamList('name'));
+        $data = $this->model->addUser($param);
+        $data = $this->convertor->commonConvertor($data);
+        $this->echoJson($data);
+    }
 
-	/**
-	 * 登录
-	 *
-	 * @param
-	 *        	string room_no 房间号
-	 * @param
-	 *        	string fullname 登录姓名
-	 * @param
-	 *        	int hotelid 物业ID
-	 * @param
-	 *        	int groupid 集团ID
-	 * @param
-	 *        	int platform 平台ID
-	 * @param
-	 *        	string identity 平台标识
-	 * @param
-	 *        	string lang 语言
-	 * @return Json
-	 */
-	public function loginAction() {
-		$param = array ();
-		$param ['room_no'] = trim ( $this->getParamList ( 'room_no' ) );
-		$param ['fullname'] = trim ( $this->getParamList ( 'fullname' ) );
-		$param ['hotelid'] = intval ( $this->getParamList ( 'hotelid' ) );
-		$param ['groupid'] = intval ( $this->getParamList ( 'groupid' ) );
-		$param ['platform'] = intval ( $this->getParamList ( 'platform' ) );
-		$param ['identity'] = trim ( $this->getParamList ( 'identity' ) );
-		$param ['lang'] = trim ( $this->getParamList ( 'lang' ) );
-		$result = $this->model->loginAction ( $param );
-		$result = $this->convertor->userInfoConvertor ( $result );
-		$this->echoSuccessData ( $result );
-	}
+    /**
+     * 登录
+     *
+     * @param
+     *            string room_no 房间号
+     * @param
+     *            string fullname 登录姓名
+     * @param
+     *            int hotelid 物业ID
+     * @param
+     *            int groupid 集团ID
+     * @param
+     *            int platform 平台ID
+     * @param
+     *            string identity 平台标识
+     * @param
+     *            string lang 语言
+     * @return Json
+     */
+    public function loginAction() {
+        $param = array();
+        $param ['room_no'] = trim($this->getParamList('room_no'));
+        $param ['fullname'] = trim($this->getParamList('fullname'));
+        $param ['hotelid'] = intval($this->getParamList('hotelid'));
+        $param ['groupid'] = intval($this->getParamList('groupid'));
+        $param ['platform'] = intval($this->getParamList('platform'));
+        $param ['identity'] = trim($this->getParamList('identity'));
+        $param ['lang'] = trim($this->getParamList('lang'));
+        $result = $this->model->loginAction($param);
+        $result = $this->convertor->userInfoConvertor($result);
+        $this->echoSuccessData($result);
+    }
 
-	/**
-	 * 根据Token获取用户信息
-	 *
-	 * @param
-	 *        	string token
-	 * @return Json
-	 */
-	public function getUserInfoByTokenAction() {
-		$token = trim ( $this->getParamList ( 'token' ) );
-		$userId = Auth_Login::getToken ( $token );
-		if (empty ( $userId )) {
-			$this->throwException ( 2, 'token验证失败' );
-		}
-		$userInfo = $this->model->getUserDetail ( $userId );
-		$userInfo ['token'] = $token;
-		$result = $this->convertor->userInfoConvertor ( $userInfo );
-		$this->echoSuccessData ( $result );
-	}
+    /**
+     * 根据Token获取用户信息
+     *
+     * @param
+     *            string token
+     * @return Json
+     */
+    public function getUserInfoByTokenAction() {
+        $token = trim($this->getParamList('token'));
+        $userId = Auth_Login::getToken($token);
+        if (empty ($userId)) {
+            $this->throwException(2, 'token验证失败');
+        }
+        $userInfo = $this->model->getUserDetail($userId);
+        $userInfo['token'] = $token;
+        $result = $this->convertor->userInfoConvertor($userInfo);
+        $this->echoSuccessData($result);
+    }
 
-	/**
-	 * 更新用户语言，切换语言的时候调用
-	 *
-	 * @param
-	 *        	string token
-	 * @param
-	 *        	int platform 平台ID
-	 * @param
-	 *        	string identity 平台标识
-	 * @param
-	 *        	string lang 语言
-	 * @return Json
-	 */
-	public function updateUserLangAction() {
-		$token = trim ( $this->getParamList ( 'token' ) );
-		$userId = Auth_Login::getToken ( $token );
-		if (empty ( $userId )) {
-			$this->throwException ( 2, 'token验证失败' );
-		}
-		$param = array ();
-		$param ['platform'] = intval ( $this->getParamList ( 'platform' ) );
-		$param ['identity'] = trim ( $this->getParamList ( 'identity' ) );
-		$param ['language'] = trim ( $this->getParamList ( 'lang' ) );
-		if (empty ( $param ['platform'] ) || empty ( $param ['identity'] ) || empty ( $param ['language'] )) {
-			$this->throwException ( 3, '入参错误' );
-		}
-		$langNameList = Enum_Lang::getLangNameList ();
-		if (! $langNameList [$param ['language']]) {
-			$this->throwException ( 5, '暂不支持该语言' );
-		}
-		$result = $this->model->updateUserById ( $param, $userId );
-		if (! $result) {
-			$this->throwException ( 4, '更新失败' );
-		}
-		$userInfo = $this->model->getUserDetail ( $userId );
-		$userInfo ['token'] = $token;
-		$result = $this->convertor->userInfoConvertor ( $userInfo );
-		$this->echoSuccessData ( $result );
-	}
+    /**
+     * 更新用户语言，切换语言的时候调用
+     *
+     * @param
+     *            string token
+     * @param
+     *            int platform 平台ID
+     * @param
+     *            string identity 平台标识
+     * @param
+     *            string lang 语言
+     * @return Json
+     */
+    public function updateUserLangAction() {
+        $token = trim($this->getParamList('token'));
+        $userId = Auth_Login::getToken($token);
+        if (empty ($userId)) {
+            $this->throwException(2, 'token验证失败');
+        }
+        $param = array();
+        $param ['platform'] = intval($this->getParamList('platform'));
+        $param ['identity'] = trim($this->getParamList('identity'));
+        $param ['language'] = trim($this->getParamList('lang'));
+        if (empty ($param ['platform']) || empty ($param ['identity']) || empty ($param ['language'])) {
+            $this->throwException(3, '入参错误');
+        }
+        $langNameList = Enum_Lang::getLangNameList();
+        if (!$langNameList [$param ['language']]) {
+            $this->throwException(5, '暂不支持该语言');
+        }
+        $result = $this->model->updateUserById($param, $userId);
+        if (!$result) {
+            $this->throwException(4, '更新失败');
+        }
+        $userInfo = $this->model->getUserDetail($userId);
+        $userInfo ['token'] = $token;
+        $result = $this->convertor->userInfoConvertor($userInfo);
+        $this->echoSuccessData($result);
+    }
+
 }
--- application/library/convertor/User.php
+++ application/library/convertor/User.php
@@ -1,89 +1,99 @@
 <?php
+
 /**
  * 用户结果转换器
  */
 class Convertor_User extends Convertor_Base {
 
-	public function __construct() {
-		parent::__construct ();
-	}
+    public function __construct() {
+        parent::__construct();
+    }
 
-	/**
-	 * 用户信息转换器
-	 * 
-	 * @param array $list
-	 *        	用户信息
-	 * @return array
-	 */
-	public function userInfoConvertor($list) {
-		$data = array ();
-		$platformNameList = Enum_Platform::getPlatformNameList ();
-		$data ['id'] = $list ['id'];
-		$data ['hotelid'] = $list ['hotelid'];
-		$data ['groupid'] = $list ['groupid'];
-		$data ['oid'] = $list ['oid'];
-		$data ['createtime'] = $list ['createtime'];
-		$data ['lastlogintime'] = $list ['lastlogintime'];
-		$data ['lastloginip'] = Util_Tools::ntoip ( $list ['lastloginip'] );
-		$data ['platform'] = $list ['platform'];
-		$data ['platformName'] = $platformNameList [$list ['platform']];
-		$data ['identity'] = $list ['identity'];
-		$data ['language'] = $list ['language'];
-		$data ['token'] = $list ['token'];
-		return $data;
-	}
+    /**
+     * 用户信息转换器
+     *
+     * @param array $list
+     *            用户信息
+     * @return array
+     */
+    public function userInfoConvertor($list) {
+        $data = array();
+        $hotelModel = new HotelListModel();
+        $hotelInfo = $hotelModel->getHotelListDetail($list['hotelid']);
+        $userModel = new UserModel();
+        $platformNameList = Enum_Platform::getPlatformNameList();
+        $data ['id'] = $list ['id'];
+        $data ['hotelid'] = $list ['hotelid'];
+        $data ['groupid'] = $list ['groupid'];
+        $data ['serviceUrl'] = $userModel->getGsmRedirect(array(
+            'PropertyInterfID' => $hotelInfo['propertyinterfid'],
+            'CustomerID' => $list['oid'],
+            'Room' => $list['room_no'],
+            'LastName' => $list['fullname'],
+        ));
+        $data ['oid'] = $list ['oid'];
+        $data ['createtime'] = $list ['createtime'];
+        $data ['lastlogintime'] = $list ['lastlogintime'];
+        $data ['lastloginip'] = Util_Tools::ntoip($list ['lastloginip']);
+        $data ['platform'] = $list ['platform'];
+        $data ['platformName'] = $platformNameList [$list ['platform']];
+        $data ['identity'] = $list ['identity'];
+        $data ['language'] = $list ['language'];
+        $data ['token'] = $list ['token'];
+        return $data;
+    }
 
-	/**
-	 * 用户信息列表
-	 * 
-	 * @param array $list
-	 *        	用户信息列表
-	 * @param int $count
-	 *        	用户总数
-	 * @param array $param
-	 *        	扩展字段
-	 * @return array
-	 */
-	public function getUserListConvertor($list, $count, $param) {
-		$data = array ('list' => array () );
-		$groupIdList = array_column ( $list, 'groupid' );
-		if ($groupIdList) {
-			$groupModel = new GroupModel ();
-			$groupInfoList = $groupModel->getGroupList ( array ('id' => $groupIdList ) );
-			$groupNameList = array_column ( $groupInfoList, 'name', 'id' );
-		}
-		$hotelIdList = array_column ( $list, 'hotelid' );
-		if ($hotelIdList) {
-			$hotelListModel = new HotelListModel ();
-			$hotelInfoList = $hotelListModel->getHotelListList ( array ('id' => $hotelIdList ) );
-			$hotelNameList = array_column ( $hotelInfoList, 'name_lang1', 'id' );
-		}
-		$platformNameList = Enum_Platform::getPlatformNameList ();
-		$languageNameList = Enum_Lang::getLangNameList ();
-		foreach ( $list as $key => $value ) {
-			$oneTemp = array ();
-			$oneTemp ['id'] = $value ['id'];
-			$oneTemp ['room_no'] = $value ['room_no'];
-			$oneTemp ['hotelid'] = $value ['hotelid'];
-			$oneTemp ['hotelName'] = $hotelNameList [$oneTemp ['hotelid']];
-			$oneTemp ['groupid'] = $value ['groupid'];
-			$oneTemp ['groupName'] = $groupNameList [$oneTemp ['groupid']];
-			$oneTemp ['oid'] = $value ['oid'];
-			$oneTemp ['fullname'] = $value ['fullname'];
-			$oneTemp ['createtime'] = $value ['createtime'];
-			$oneTemp ['lastlogintime'] = $value ['lastlogintime'];
-			$oneTemp ['lastloginip'] = $value ['lastloginip'];
-			$oneTemp ['platform'] = $value ['platform'];
-			$oneTemp ['platformName'] = $platformNameList [$oneTemp ['platform']];
-			$oneTemp ['identity'] = $value ['identity'];
-			$oneTemp ['language'] = $value ['language'];
-			$oneTemp ['languageName'] = $languageNameList [$oneTemp ['language']];
-			$data ['list'] [] = $oneTemp;
-		}
-		$data ['total'] = $count;
-		$data ['page'] = $param ['page'];
-		$data ['limit'] = $param ['limit'];
-		$data ['nextPage'] = Util_Tools::getNextPage ( $data ['page'], $data ['limit'], $data ['total'] );
-		return $data;
-	}
+    /**
+     * 用户信息列表
+     *
+     * @param array $list
+     *            用户信息列表
+     * @param int $count
+     *            用户总数
+     * @param array $param
+     *            扩展字段
+     * @return array
+     */
+    public function getUserListConvertor($list, $count, $param) {
+        $data = array('list' => array());
+        $groupIdList = array_column($list, 'groupid');
+        if ($groupIdList) {
+            $groupModel = new GroupModel ();
+            $groupInfoList = $groupModel->getGroupList(array('id' => $groupIdList));
+            $groupNameList = array_column($groupInfoList, 'name', 'id');
+        }
+        $hotelIdList = array_column($list, 'hotelid');
+        if ($hotelIdList) {
+            $hotelListModel = new HotelListModel ();
+            $hotelInfoList = $hotelListModel->getHotelListList(array('id' => $hotelIdList));
+            $hotelNameList = array_column($hotelInfoList, 'name_lang1', 'id');
+        }
+        $platformNameList = Enum_Platform::getPlatformNameList();
+        $languageNameList = Enum_Lang::getLangNameList();
+        foreach ($list as $key => $value) {
+            $oneTemp = array();
+            $oneTemp ['id'] = $value ['id'];
+            $oneTemp ['room_no'] = $value ['room_no'];
+            $oneTemp ['hotelid'] = $value ['hotelid'];
+            $oneTemp ['hotelName'] = $hotelNameList [$oneTemp ['hotelid']];
+            $oneTemp ['groupid'] = $value ['groupid'];
+            $oneTemp ['groupName'] = $groupNameList [$oneTemp ['groupid']];
+            $oneTemp ['oid'] = $value ['oid'];
+            $oneTemp ['fullname'] = $value ['fullname'];
+            $oneTemp ['createtime'] = $value ['createtime'];
+            $oneTemp ['lastlogintime'] = $value ['lastlogintime'];
+            $oneTemp ['lastloginip'] = $value ['lastloginip'];
+            $oneTemp ['platform'] = $value ['platform'];
+            $oneTemp ['platformName'] = $platformNameList [$oneTemp ['platform']];
+            $oneTemp ['identity'] = $value ['identity'];
+            $oneTemp ['language'] = $value ['language'];
+            $oneTemp ['languageName'] = $languageNameList [$oneTemp ['language']];
+            $data ['list'] [] = $oneTemp;
+        }
+        $data ['total'] = $count;
+        $data ['page'] = $param ['page'];
+        $data ['limit'] = $param ['limit'];
+        $data ['nextPage'] = Util_Tools::getNextPage($data ['page'], $data ['limit'], $data ['total']);
+        return $data;
+    }
 }
